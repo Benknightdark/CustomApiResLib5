af5857a4018b5debf94f26103675f6a399147a4d
ben <moon791017@gmail.com>
2021-12-28 17:16:15 +0800

    bugfix

--- Models/CustomResponseModel.cs
+++ Models/CustomResponseModel.cs
@@ -6,10 +6,10 @@ namespace CutomApiLib.Models
 {
     public class CustomResponseModel
     {
-        public object errors { get; set; }
-        public string title { get; set; }
-        public int status { get; set; }
-        public string traceId { get; set; }
-        public object data { get; set; }
+        public object? errors { get; set; }
+        public string? title { get; set; }
+        public int? status { get; set; }
+        public string? traceId { get; set; }
+        public object? data { get; set; }
     }
 }
